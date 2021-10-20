4d4e64140977ea82380d9220006f6357c5ba19e2
Ian Greenblott <imgreenblott@gmail.com>
2021-10-19 23:16:48 -0400

    Added function for five day forecast to script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var APIKey = "a0b0ce6f50e0b65130382561e6a81e57";
-console.log(APIKey);
+var APIKeyTwo = "fa0e5b8eca9488544f4571b9572e84d3";
+console.log(APIKey, APIKeyTwo);
 var city = document.getElementById("citySearch");
 console.log(city);
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
@@ -17,8 +18,22 @@ function concludeSearch(event) {
     }).then(function(data){
         console.log(data);
     });
+fiveDayFore();
+}
+
+function fiveDayFore() {
+  
+    console.log(city.value);
+    var queryURLfive = "http://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=" + APIKeyTwo;
+    console.log(queryURLfive);
+    fetch(queryURLfive).then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+    });
 
 }
+
 
 var buttonSearch = document.getElementById("searchBtn");
 
