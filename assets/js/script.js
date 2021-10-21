507af195d87a1816f3d4e1b2ab93d4321bbda954
Ian Greenblott <imgreenblott@gmail.com>
2021-10-20 20:49:22 -0400

    Added functions and loops to improve functionality of website.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var APIKeyTwo = "fa0e5b8eca9488544f4571b9572e84d3";
 console.log(APIKey, APIKeyTwo);
 var city = document.getElementById("citySearch");
 console.log(city);
+//Grabbing value from LS if no vlaue is present assign a blank array instead. 
+var localStorageGrab = JSON.parse(localStorage.getItem('cityList')) || [];
+console.log("LS array", localStorageGrab);
+
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 // console.log(queryURL);
 
@@ -13,27 +17,72 @@ function concludeSearch(event) {
     console.log(city.value);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + APIKey;
     console.log(queryURL);
-    fetch(queryURL).then(function(response){
+    fetch(queryURL).then(function (response) {
         return response.json();
-    }).then(function(data){
+    }).then(function (data) {
         console.log(data);
+        fiveDayFore();
+        //Save it to LOCAL STORAGE 
+        //previous searched citites 
+        localStorageGrab.push(city.value);
+        console.log("Appending values to exisiting list", localStorageGrab);
+        localStorage.setItem('cityList', JSON.stringify(localStorageGrab))
+        //re-render the cityloop 
+        cityLoop();
     });
-fiveDayFore();
+
 }
 
 function fiveDayFore() {
-  
+
     console.log(city.value);
     var queryURLfive = "http://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=" + APIKeyTwo;
     console.log(queryURLfive);
-    fetch(queryURLfive).then(function(response){
+    fetch(queryURLfive).then(function (response) {
         return response.json();
-    }).then(function(data){
+    }).then(function (data) {
         console.log(data);
     });
-
 }
 
+//Render the City List buttons 
+function cityLoop() {
+    console.log("Previous searched cities", localStorageGrab.length);
+    var divElement = document.getElementById("cityListEl");
+    var counter = 0;
+    //reset the div 
+    divElement.innerHTML = '';
+
+
+    if (localStorageGrab.length <= 5) {
+        
+        counter = localStorageGrab.length;
+    } else {
+        //in case array length is > than 5 set the counter MAX value as 5 
+        counter = 5;
+    }
+
+    //loop to run max 5 times or lesser depending on the array values 
+    for (i = 0; i < counter; i++) {
+        //grabs each index from an arra
+        var arrElement = localStorageGrab[i];
+        console.log("City is ", arrElement);
+
+        //creating a tag html element
+        var aTagElement = document.createElement("a");
+      
+        aTagElement.setAttribute("class", "list-group-item list-group-item-action");
+        aTagElement.textContent = arrElement;
+        console.log("Create a tag", aTagElement);
+
+        divElement.append(aTagElement);
+
+    }
+}
+
+
+//Page load 
+cityLoop();
 
 var buttonSearch = document.getElementById("searchBtn");
 
