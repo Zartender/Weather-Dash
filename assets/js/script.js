081d530c8a042356bbb0f4965b9c89725bf25931
Ian Greenblott <imgreenblott@gmail.com>
2021-12-14 08:31:46 -0500

    Added parse response and uv index response generator.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+function pageInit() {  
+  
     const cityEl = document.getElementById("enter-city");
     const searchEl = document.getElementById("search-button");
     const clearEl = document.getElementById("clear-history");
@@ -19,12 +21,59 @@
     const APIKeyTwo = "fa0e5b8eca9488544f4571b9572e84d3";
     console.log(APIKey, APIKeyTwo);
 
-    function pageInit() {};
+    
+
+        function getWeather(cityName) {
+            // Execute a current weather get request from open weather api
+            let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+            axios.get(queryURL)
+                .then(function (response) {
+    
+                    todayweatherEl.classList.remove("d-none");
+    
+                    // Parse response to display current weather
+                    const currentDate = new Date(response.data.dt * 1000);
+                    const day = currentDate.getDate();
+                    const month = currentDate.getMonth() + 1;
+                    const year = currentDate.getFullYear();
+                    nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+                    let weatherPic = response.data.weather[0].icon;
+                    currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                    currentPicEl.setAttribute("alt", response.data.weather[0].description);
+                    currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+                    currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+                    currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
+                     // Get UV Index
+                let lat = response.data.coord.lat;
+                let lon = response.data.coord.lon;
+                let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+                axios.get(UVQueryURL)
+                    .then(function (response) {
+                        let UVIndex = document.createElement("span");
+                        
+                        // When UV Index is good, shows green, when ok shows yellow, when bad shows red
+                        if (response.data[0].value < 4 ) {
+                            UVIndex.setAttribute("class", "badge badge-success");
+                        }
+                        else if (response.data[0].value < 8) {
+                            UVIndex.setAttribute("class", "badge badge-warning");
+                        }
+                        else {
+                            UVIndex.setAttribute("class", "badge badge-danger");
+                        }
+                        console.log(response.data[0].value)
+                        UVIndex.innerHTML = response.data[0].value;
+                        currentUVEl.innerHTML = "UV Index: ";
+                        currentUVEl.append(UVIndex);
+                    });
+                });
+            }
+        
+
+           
 
 
 
-
-buttonSearch.addEventListener("click", concludeSearch);
-
-
-
+                }
+pageInit();
